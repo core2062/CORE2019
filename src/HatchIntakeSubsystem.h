1b15bd621ffb58f6d48d76f89d9401c20d76087b
Sean McGuire <smcguire000@stu.waukesha.k12.wi.us>
2019-01-26 16:39:15 -0600

    Added ports to robot.h and body code to Hatch Intake Subsystem. Added HatchIntakeOff function, motor and solenoid objects.

--- src/HatchIntakeSubsystem.h
+++ src/HatchIntakeSubsystem.h
@@ -2,18 +2,27 @@
 
 #include "ctre/Phoenix.h"
 #include <CORERobotLib.h>
+#include <frc/WPILib.h>
+
 
 using namespace CORE;
 
 class HatchIntakeSubsystem : public CORESubsystem {
 public:
 	HatchIntakeSubsystem();
-	void robotInit();
-	void teleopInit();
-	void teleop();
-	void SetIntakeWheelSpeed(double speed);
-	void ActuateHatchIntake();
+	void robotInit() override;
+	void teleopInit() override;
+	void teleop() override;
+	void SetIntakeOn();
+	void ToggleHatchIntake();
+	void SetIntakeOff();
 	bool GetHatchState();
+	bool isDown;
+
+	frc::DigitalInput m_photoeye;
+	TalonSRX m_hatchIntake;
+
+
 private:
-	
+	frc::DoubleSolenoid m_hatchIntakeSolenoid;
 };
