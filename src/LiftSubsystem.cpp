d0194919073a8d1a63fef8b299ac17fdfc96f1ce
unknown <50kite17050buster130@gmail.com>
2019-01-28 14:53:32 -0600

    Made changes to the lift subsystem **DOES NOT COMPILE - DO NOT MERGE!**

--- src/LiftSubsystem.cpp
+++ src/LiftSubsystem.cpp
@@ -1,9 +1,17 @@
 #include <LiftSubsystem.h>
+#include <frc/WPILib.h>
+#include <Robot.h>
 
-#include "Robot.h"
+using namespace CORE;
 
-
-LiftSubsystem::LiftSubsystem() {
+LiftSubsystem::LiftSubsystem() : m_firstLevelHatch("First Level Hatch Height", 0.0),
+                                 m_firstLevelCargo("First Level Cargo Height", 0.0),
+                                 m_secondLevelHatch("Second Level Hatch Height", 0.0),
+                                 m_secondLevelCargo("Second Level Cargo Height", 0.0),
+                                 m_thirdLevelHatch("Third Level Hatch Height", 0.0),
+                                 m_thirdLevelCargo("Third Level Cargo Height", 0.0),
+                                 m_rightLiftMotor(16),
+                                 m_rightLiftMotor(17) {
 
 }
 
@@ -37,10 +45,33 @@ double LiftSubsystem::GetLiftPosition(){
 }
 
 double LiftSubsystem::GetLiftInches(){
-
+    // return m_liftInches;
 }
 
 void LiftSubsystem::ResetEncoder(){
 
 }
-    
+
+void LiftSubsystem::SetFirstLevelHatchHeight() {
+    SetRequestedPosition(m_firstLevelHatch.Get());
+}
+
+void LiftSubsystem::SetSecondLevelHatchHeight() {
+    SetRequestedPosition(m_secondLevelHatch.Get());
+}
+
+void LiftSubsystem::SetThirdLevelHatchHeight() {
+    SetRequestedPosition(m_thirdLevelHatch.Get());
+}
+
+void LiftSubsystem::SetFirstLevelCargoHeight() {
+    SetRequestedPosition(m_firstLevelHatch.Get());
+}
+
+void LiftSubsystem::SetSecondLevelCargoHeight() {
+    SetRequestedPosition(m_secondLevelHatch.Get());
+}
+
+void LiftSubsystem::SetThirdLevelCargoHeight() {
+    SetRequestedPosition(m_thirdLevelHatch.Get());
+}
--- src/LiftSubsystem.h
+++ src/LiftSubsystem.h
@@ -1,22 +1,33 @@
 #pragma once
 
-#include "ctre/Phoenix.h"
+#include <ctre/Phoenix.h>
+#include <frc/WPILib.h>
 #include <CORERobotLib.h>
+#include <COREUtilities/COREConstant.h>
 
-using namespace CORE;
+using namespace frc;
 
 class LiftSubsystem : public CORESubsystem {
 public:
 	LiftSubsystem();
-	void robotInit();
-	void teleopInit();
-	void teleop();
+	void robotInit() override;
+	void teleopInit() override;
+	void teleop() override;
 	void SetLift(double speed);
 	void SetRequestedPosition(double position);
 	void SetRequestedSpeed(double speed);
+	void SetFirstLevelHatchHeight();
+	void SetSecondLevelHatchHeight();
+	void SetThirdLevelHatchHeight();
+	void SetFirstLevelCargoHeight();
+	void SetSecondLevelCargoHeight();
+	void SetThirdLevelCargoHeight();
 	double GetLiftPosition();
 	double GetLiftInches();
 	void ResetEncoder();
 private:
-	
+	double heightMultiplier;
+	COREConstant<double> m_firstLevelHatch, m_firstLevelCargo, m_secondLevelHatch, m_secondLevelScorer,
+		m_thirdLevelHatch, m_thirdLevelScorer;
+	TalonSRX m_rightLiftMotor, m_leftLiftMotor;
 };
