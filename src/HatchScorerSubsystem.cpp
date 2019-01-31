304f06afdb79e88f72ea11ff942a6822b3612f19
COREProgrammers <core2062programmers@googlegroups.com>
2019-01-31 16:29:50 -0600

    Added Sudocode, filled in basic bits.

--- src/HatchScorerSubsystem.cpp
+++ src/HatchScorerSubsystem.cpp
@@ -1,11 +1,11 @@
 #include <HatchScorerSubsystem.h>
-
+#include <frc/WPILib.h>
 #include "Robot.h"
 
-//TODO:Fill these in with actuall port numbers
-HatchScorerSubsystem::HatchScorerSubsystem() :m_hatchScorerSolenoid(0, 0, 0)
-
-{
+//TODO:Fill these in with actual port numbers
+HatchScorerSubsystem::HatchScorerSubsystem() : m_solenoidExtend(0, 0, 0),
+                                               m_solenoidGrab(0, 0, 0),
+                                               m_solenoidRelease(0, 0, 0) {
 
 }
 
