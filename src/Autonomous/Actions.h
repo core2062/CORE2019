71e2cbc6f94921bea573f8434c8865bde2bf7502
Sean McGuire <smcguire000@stu.waukesha.k12.wi.us>
2019-02-17 14:35:29 -0600

    Actions.h commit for sean.

---
+++ src/Autonomous/Actions.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "DriveDistanceAction.h"
+#include "DriveWaypointAction.h"
+#include "GrabCargoAction.h"
+#include "GrabHatchAction.h"
+#include "PlaceCargoAction.h"
+#include "PlaceHatchAction.h"
+#include "RaiseLiftAction.h"
