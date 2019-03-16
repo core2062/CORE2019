66539300c8cc7f1890e749ac6ec086269d1e73fb
unknown <50kite17050buster130@gmail.com>
2019-03-16 08:48:13 -0500

    Changes to hatch top rocket near

--- src/Autonomous/HatchTopRocketNear.cpp
+++ src/Autonomous/HatchTopRocketNear.cpp
@@ -7,9 +7,10 @@ HatchTopRocketNear::HatchTopRocketNear() :
 }
 
 void HatchTopRocketNear::AddNodes() {
-    m_driveForwardToRocket = new Node(5, new DriveWaypointAction(TankPath::FromFile("path.json", false)), new RaiseLiftAction(ThirdLevelHatch));
-    m_lowerLiftToFirstLevel = new Node(5, new RaiseLiftAction(FirstLevelHatch));
-    m_placeHatchOnTopRocket = new Node(5, new PlaceHatchAction(CLOSE));
+    m_driveForwardToRocket = new Node(5, new DriveWaypointAction(TankPath::FromFile("path.json", false)), new RaiseLiftAction(RaiseLift::ThirdLevelHatch)
+        , new PlaceHatchAction(PlaceHatch::EXTEND));
+    m_lowerLiftToFirstLevel = new Node(5, new RaiseLiftAction(RaiseLift::FirstLevelHatch));
+    m_placeHatchOnTopRocket = new Node(5, new PlaceHatchAction(PlaceHatch::CLOSE));
     AddFirstNode(m_driveForwardToRocket);
     m_driveForwardToRocket->AddNext(m_placeHatchOnTopRocket);
     m_placeHatchOnTopRocket->AddNext(m_lowerLiftToFirstLevel);
