91778fba5fae4512a8d2f8f087b6e841ecad70bc
unknown <50kite17050buster130@gmail.com>
2019-03-16 09:11:32 -0500

    Deleted unnecessary auton routines

--- src/Autonomous/HatchFrontBay.cpp
+++ src/Autonomous/HatchFrontBay.cpp
@@ -7,7 +7,7 @@ HatchFrontBay::HatchFrontBay() :
 
 void HatchFrontBay::AddNodes() {
     m_driveForwardToFrontCargoBay = new Node(10, new DriveWaypointAction(TankPath::FromFile("path.json", false)));
-    m_placeHatchOnFrontCargoBay = new Node(10, new PlaceHatchAction(CLOSE));
+    m_placeHatchOnFrontCargoBay = new Node(10, new PlaceHatchAction(PlaceHatch::CLOSE));
     AddFirstNode(m_driveForwardToFrontCargoBay);
     m_driveForwardToFrontCargoBay->AddNext(m_placeHatchOnFrontCargoBay);
 }
